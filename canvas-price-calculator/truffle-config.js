e5d2b61fa780d340a70a78234799eb8a920968a6
Shawn Potter <potter.shawn@student.greenriver.edu>
2022-04-20 13:05:00 -0700

    Updated truffle-config for Rinkey Test Network

--- canvas-price-calculator/truffle-config.js
+++ canvas-price-calculator/truffle-config.js
@@ -57,6 +57,18 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider({
+        privateKeys: [process.env.PRIVATE_KEY_1],
+        providerOrUrl: process.env.INFURA_API_URL,
+        numberOfAddresses: 1
+      }),
+      network_id: 4,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true 
+    },
     // ropsten: {
     // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
     // network_id: 3,       // Ropsten's id
