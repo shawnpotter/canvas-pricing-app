217c8b6e706b13dfec953bc8c63d0429183b42db
Shawn Potter <potter.shawn@student.greenriver.edu>
2022-04-20 13:28:33 -0700

    fixed indentation on index.js

--- pricing-app/pages/index.js
+++ pricing-app/pages/index.js
@@ -1,58 +1,59 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
       <Head>
-          <title>Calculator App</title>
-          <meta name="description" content="A blockchain pricing tool" />
-
+        <title>Calculator App</title>
+        <meta name="description" content="A blockchain pricing tool" />
       </Head>
       <div className='container'>
           <nav className='navbar mt-4'>
-              <div>
-                  <div className='navbar-brand'>
-                      <h1>Calculator App</h1>
-                  </div>
-              </div>
-              <div className='navbar-end'>
-                  <button /* onClick={walletHandler} */ className='btn btn-primary'>Connect Wallet</button>
-              </div>
+            <div>
+                <div className='navbar-brand'>
+                    <h1>Calculator App</h1>
+                </div>
+            </div>
+            <div className='navbar-end'>
+                <button /* onClick={walletHandler} */ className='btn btn-primary'>Connect Wallet</button>
+            </div>
           </nav>
           <section>
-              <div className='container'>
-                  <p>Result: placeholder text {/* {result} */}</p>
-              </div>
+            <div className='container'>
+                <p>Result: placeholder text {/* {result} */}</p>
+            </div>
           </section>
           <section>
-              <div className='container text-danger'>
-                  <p>{/* {error} */}</p>
-              </div>
+            <div className='container text-danger'>
+                <p>{/* {error} */}</p>
+            </div>
           </section>
           <div>
-              <form className='calculatorForm'>
-                  <div>
-                      <label className='form-label'> Width:
-                          <input /* onChange={updateWidth} */ className='form-control' type={"text"} name={"width"} />
-                      </label>
-                  </div>
-                  <div>
-                      <label className='form-label'> Height:
-                          <input /* onChange={updateHeight} */ className='form-control' type={"text"} name={"height"} />
-                      </label>
-                  </div>
-                  <div>
-                      <label className='form-label'> Price per Square Inch:
-                          <input /* onChange={updatePrice} */ className='form-control' type={"text"} name={"squareInch"} />
-                      </label>
-                  </div>
-              </form>
-              <div>
-                  <button className='btn btn-success'  /* onClick={getCalculationHandler} */>Submit</button>
-              </div>
-          </div>
+            <form className='calculatorForm'>
+                <div>
+                    <label className='form-label'> Width:
+                        <input /* onChange={updateWidth} */ className='form-control' type={"text"} name={"width"} />
+                    </label>
+                </div>
+                <div>
+                    <label className='form-label'> Height:
+                        <input /* onChange={updateHeight} */ className='form-control' type={"text"} name={"height"} />
+                    </label>
+                </div>
+                <div>
+                    <label className='form-label'> Price per Square Inch:
+                        <input /* onChange={updatePrice} */ className='form-control' type={"text"} name={"squareInch"} />
+                    </label>
+                </div>
+            </form>
+            <div>
+                <button className='btn btn-success'  /* onClick={getCalculationHandler} */>Submit</button>
+            </div>
+        </div>
       </div>
-  </div>
+    </div>
   )
 }
+
+export default Home;
