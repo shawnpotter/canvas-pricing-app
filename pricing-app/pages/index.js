808537a21e40f5d78fa19e2a19c62bf21b3dbab7
Shawn Potter <potter.shawn@student.greenriver.edu>
2022-04-30 12:59:56 -0700

    Added reconnection to wallet on page reload

--- pricing-app/pages/index.js
+++ pricing-app/pages/index.js
@@ -1,19 +1,27 @@
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Web3 from 'web3';
+import {useWeb3React} from "@web3-react/core"
 import calculatorContract from '../blockchain/calculator';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { injected } from '../components/connectors/connectors';
 
 const Home = () => {
 
-  //Constant useState variables with setters 
+  const { activate, deactivate } = useWeb3React();
+
+  //React hooks
   const [error, setError] = useState('');
   const [result, setResult] = useState('');
   const [height, setHeight] = useState('');
   const [width, setWidth] = useState('');
   const [price, setPrice] = useState('');
-  const [address, setAddress] = useState(''); //address is never used because we don't send transactions in this but require it for MetaMask integration 
+  //const [address, setAddress] = useState(''); //address is never used because we don't send transactions in this but require it for MetaMask integration 
   const [localContract, setLocalContract] = useState('');
+  //const [connectWallet, setConnectWallet] = useState('Connect Wallet');
+  //const [disconnectWallet, setDisconnectWallet] = useState('Disconnect');
+  const [statusBtn, setStatusBtn] = useState('Hidden');
+  const [connectBtnHidden, setConnecBtnHidden] = useState('');
 
   //Updaters for the form inputs that update the values typed into the form fields.
   const updateHeight = event => {
@@ -42,21 +50,81 @@ const Home = () => {
     }
   }
 
+/*   useEffect(()=> {
+  }); */
+
+  useEffect(() => {
+    const connectWalletOnLoad = async () => {
+      if(localStorage?.getItem('isWalletConnected')) {
+        try {
+          await activate(injected);
+          createLocalContract();
+          localStorage.setItem('isWalletConnected', true);
+          switchButtons(localStorage?.getItem('isWalletConnected'))
+        }
+        catch(err) {
+          console.log(err)
+        }
+      }
+    }
+    connectWalletOnLoad();
+  }, []);
+
+function switchButtons(isWalletConnected) {
+  if(isWalletConnected) {
+    setConnecBtnHidden('Hidden');
+    setStatusBtn('');
+  } else {
+    setConnecBtnHidden('');
+    setStatusBtn('Hidden');
+  }
+}
+
+async function connect(){
+  try {
+    const accounts = await activate(injected);
+    createLocalContract();
+    localStorage.setItem('isWalletConnected', true);
+    switchButtons(localStorage?.getItem('isWalletConnected'))
+  } 
+  catch (err) {
+    console.log(err);
+  }
+}
+
+async function disconnect() {
+  try {
+    deactivate();
+    localStorage.setItem('isWalletConnected', false);
+    switchButtons(localStorage?.getItem('isWalletConnected'))
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+function createLocalContract(){
+    //set Web3 Instance
+    const web3 = new Web3(window.ethereum);
+
+    //create local contract copy
+    const calculator = calculatorContract(web3);
+    setLocalContract(calculator);
+}
+
   /* Create wallet handler to connect to browser wallet */
-  const walletHandler = async () => {
+ /*  const walletHandler = async () => {
+    //check to see if metamask is installed
     if(typeof window !== 'undefined' & typeof window.ethereum !== 'undefined') {
       //try to connect to wallet
       try {
-        await window.ethereum.request({ method: "eth_requestAccounts" });
-
-        //set Web3 Instance
-        const web3 = new Web3(window.ethereum);
-
-        //get account addresses
-        const accounts = await web3.eth.getAccounts();
-
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        
         //set Address
         setAddress(accounts[0]);
+        
+        //set Web3 Instance
+        const web3 = new Web3(window.ethereum);
 
         //create local contract copy
         const calculator = calculatorContract(web3);
@@ -68,7 +136,7 @@ const Home = () => {
     } else {
       console.log('please install metamask')
     }
-  }
+  } */
 
 
   //Render for the web page
@@ -86,7 +154,8 @@ const Home = () => {
                 </div>
             </div>
             <div className='navbar-end'>
-                <button onClick={walletHandler} className='btn btn-primary'>Connect Wallet</button>
+                <button onClick={connect} className='btn btn-primary' hidden={connectBtnHidden}>Connect Wallet</button>
+                <button className='btn btn-primary' hidden={statusBtn} disabled>Connected</button>
             </div>
           </nav>
           <section>
